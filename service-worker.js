2154b0ae52610a2574e8a5424b5cec5c34cfd480
Tx-Nascimento <tiagonascimento1123@gmail.com>
2026-04-19 11:23:44 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,20 @@
-self.addEventListener('install', () => {
-  console.log('Service Worker instalado');
+const CACHE = "paradas-v2";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', () => {
-  // simples (sem cache por enquanto)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
